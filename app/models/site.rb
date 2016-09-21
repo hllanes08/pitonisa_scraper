c81bc4d551e3fdfe2ecdc4527dcbc33393d6ae3e
Harvin Llanes <harvin.llanes@gmail.com>
2016-09-21 12:57:59 -0600

    search occurrences by word

--- app/models/site.rb
+++ app/models/site.rb
@@ -23,20 +23,31 @@ class Site < ActiveRecord::Base
 	search(site, "h2", tags)
 	search(site, "h3", tags)
 	search(site, "h4", tags)
-
-	return tags
+	trends =  Hash.new
+	tags.each do |tag|
+	    tag.split(" ").each do |split_word|
+		next unless split_word.length > 4
+		if trends.key?(split_word)
+		    trends[split_word] += 1
+		else
+		    trends[split_word] = 1
+		end
+	    end	   
+	end
+	return trends.sort_by {|key, value| value}.reverse.to_h
     end
 
     private
 
     def search(site, tag, tags)
 	site.css(tag).each do |item|
-		content =  item.text
-		content = content.gsub(/\t/,'')
-		content = content.gsub(/\n/,'')
-		content = content.gsub(/\r/,'')
-		tags.push content
-	end
+     	            content =  item.text
+		    content = content.gsub(/\t/,'')
+		    content = content.gsub(/\n/,'')
+		    content = content.gsub(/\r/,'') 
+		    tags.push content
+	
+	    end
 	return tags
     end
 end
