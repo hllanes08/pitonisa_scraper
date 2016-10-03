4d04ff0d4879cd66276e5a290f38bae493e15a7a
Harvin Llanes <harvin.llanes@gmail.com>
2016-10-03 13:46:14 -0600

    Update to rails 5

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -63,10 +62,9 @@ ActiveRecord::Schema.define(version: 20160921050514) do
     t.datetime "updated_at",                          null: false
     t.string   "auth_token"
     t.datetime "expires_token_at"
+    t.index ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
-
-  add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
