a8e3f5afdd884c6e348ea5a1fdf69c0c2442f2f2
Harvin Llanes <harvin.llanes@gmail.com>
2016-10-11 17:37:40 -0600

    Group by tag

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -2,7 +2,10 @@ class API::V1::SearchesController < ApplicationController
     before_action :authenticate_with_token!, only: [:index]
 
     def index
-  	search = Search.where(user_id:current_user.id)
-        render json: Search.all	
+	    searches = []
+	    Search.where(user_id:current_user.id).group_by(&:tag).each do |tag, records|
+		    searches.push [ tag: tag, value: records.count ]
+    	    end
+        render json: searches
     end
 end
