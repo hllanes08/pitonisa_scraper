381b2823782017db6bc5267e00450c2a00d94648
Harvin LLanes <harvin.llanes@gmail.com>
2016-10-13 14:25:13 -0600

    Dummi change

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -2,6 +2,7 @@ class API::V1::SearchesController < ApplicationController
     before_action :authenticate_with_token!, only: [:index]
 
     def index
+
 	    searches = []
 	    Search.where(user_id:current_user.id).group_by(&:tag).each do |tag, records|
 		    searches.push [ tag: tag, value: records.count ]
